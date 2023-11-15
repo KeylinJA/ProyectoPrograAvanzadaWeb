8ab3630d20ee8521d0475e01ff41bd879ee03703
Keylin <kjimenez40815@ufide.ac.cr>
2023-11-15 02:41:04 -0600

    Commit Inicial
    Se subió el proyecto como su versión inicial

---
+++ ProyectoWEB/ProyectoWEB/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoWEB.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+		public IActionResult RegistrarCuenta()
+		{
+			return View();
+		}
+	}
+}
+
