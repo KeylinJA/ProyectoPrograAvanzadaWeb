f7a6e390711b8dfd3efec093bb951a2e45d6ecb6
hramirez3028 <hramirez30130@ufide.ac.cr>
2023-12-06 23:01:20 -0600

    Cambio de Producto a Instrumento

---
+++ ProyectoAPI/ProyectoAPI/Entities/ProfesorEnt.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoAPI.Entities
+{
+    public class ProfesorEnt
+    {
+        public long IdProfesor { get; set; }
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+
+    }
+}
