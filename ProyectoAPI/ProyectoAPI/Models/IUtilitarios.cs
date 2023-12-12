4b726eb7bc6ed0e2b97a3ccf05a019cbda039d54
Jocksan Cubero <72951230+JocksanCubero@users.noreply.github.com>
2023-12-12 01:46:59 -0600

    Cambios Carrito

---
+++ ProyectoAPI/ProyectoAPI/Models/IUtilitarios.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace ProyectoAPI.Models
+{
+    public interface IUtilitarios
+    {
+        public string Encrypt(string texto);
+
+        public string Decrypt(string texto);
+
+        public long ObtenerUsuario(IEnumerable<Claim> valores);
+    }
+}
